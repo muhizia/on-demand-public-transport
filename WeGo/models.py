62a3dfcc9b974fb750e864788e21b34b7d19df16
Patrick DUSHIMIMANA <dushimimanapatrick@gmail.com>
2020-07-26 00:21:57 +0200

    created a separate model of user and make it abstract so that other model can inherite from it

--- WeGo/models.py
+++ WeGo/models.py
@@ -1,41 +1,39 @@
 from django.db import models
 from location_field.models.plain import PlainLocationField
+from django.contrib.auth.models import User
 
-
-
-# class for Passengers' account
-class Manager(models.Model):
-    id              = models.AutoField(primary_key = True)
-    firstName       = models.CharField(max_length=100,null=True, blank=True)
-    lastName        = models.CharField(max_length=100,null=True, blank=True)
+class User(models.Model):
     userName        = models.CharField(max_length=100, unique= True)
-    telephone       = models.CharField(max_length=11)
-    address         = models.CharField (max_length= 100,blank=True)
-    email           = models.EmailField(max_length=100,unique= True,null=True, blank=True)
+    email           = models.EmailField(max_length=100,null=True, blank=True)
     password        = models.CharField(max_length= 10)
     passwordConfirm = models.CharField(max_length=10)
+    
+    class Meta:
+        abstract = True
+
+# class for Passengers' account
+class Manager(User):
+    firstName       = models.CharField(max_length=100,null=True, blank=True)
+    lastName        = models.CharField(max_length=100,null=True, blank=True)
+    telephone       = models.CharField(max_length=11)
+    address         = models.CharField (max_length= 100,blank=True)
     profilePicture  = models.FileField(max_length=None,blank=True,null=True)
+    roles           = models.CharField(default="manager",max_length= 50)
     # this method is used as toString for java.
     def __str__(self):
-        return  self.name
+        return  self.firstName+' '+lastName
 
-    def clean(self):
-    	if self.name:
-    		self.name = self.name.strip()
+  
     
 
 # class for Passengers' account
-class Passenger(models.Model):
-    id              = models.AutoField(primary_key = True)
+class Passenger(User):
     firstName       = models.CharField(max_length=100,null=True, blank=True)
     lastName        = models.CharField(max_length=100,null=True, blank=True)
-    userName        = models.CharField(max_length=100, unique= True)
     telephone       = models.CharField(max_length=11)
     address         = models.CharField (max_length= 100,blank=True)
-    email           = models.EmailField(max_length=100,unique= True,null=True, blank=True)
-    password        = models.CharField(max_length= 10)
-    passwordConfirm = models.CharField(max_length=10)
     profilePicture  = models.FileField(max_length=None,blank=True,null=True)
+    roles            = models.CharField(default="passenger", max_length= 50)
     # this method is used as toString for java.
     def __str__(self):
         return self.firstName +' '+ self.lastName
@@ -43,17 +41,13 @@ class Passenger(models.Model):
     
 
 # class for Driver account
-class Driver(models.Model):
-    id              = models.AutoField(primary_key = True)
+class Driver(User):
     firstName       = models.CharField(max_length=100)
     lastName        = models.CharField(max_length=100)
-    userName        = models.CharField(max_length=100, unique= True)
     telephone       = models.CharField(max_length=11)
     address         = models.CharField (max_length= 100,blank=True)
-    email           = models.EmailField(max_length=100,unique= True)
-    password        = models.CharField(max_length= 10)
-    passwordConfirm = models.CharField(max_length=10)
     profilePicture  = models.FileField(max_length=None,blank=True,null=True)
+    roles            = models.CharField(default="driver",max_length=50)
     def __str__(self):
         return self.firstName +' '+ self.lastName
 
@@ -78,13 +72,35 @@ class Bus(models.Model):
         if self.plateNumber:
             self.plateNumber = self.plateNumber.strip()
 
+
+class Route(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class BusStop(models.Model):
+
+    routes = models.ForeignKey(Route, on_delete=models.CASCADE)
+    busStopName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.busStopName
+
+    class Meta:
+        unique_together = (("routes", "busStopName"),) 
+
+        
 # class for requesting a ride
 class RideRequest(models.Model):
+    CHOICE = [BusStop.objects.all()]
     pickupTime          = models.DateTimeField(blank=True, null = True, default=None)
     departureCity       = models.CharField(max_length=255)
-    departureLocation   = PlainLocationField(based_fields=['departureCity'], zoom=2)
+    departureLocation   = PlainLocationField(based_fields=['departureCity'], zoom=13)
     destinationCity     = models.CharField(max_length=255)
-    destinationLocation = PlainLocationField(based_fields=['destinationCity'], zoom=2)
+    destinationLocation = PlainLocationField(based_fields=['destinationCity'], zoom=13)
     numberOfSeets       = models.PositiveIntegerField(null=False)
     disabledPoeple      = models.PositiveIntegerField(null=True)
     passengers          = models.ManyToManyField(Passenger, blank = True) 
+
+
